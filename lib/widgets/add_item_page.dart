52c2517fb8618ac28cfb5397e94cd32b7a91a664
stoian2662 <stoian2662@gmail.com>
2021-12-21 20:40:07 +0200

    Add item form design changed.

--- lib/widgets/add_item_page.dart
+++ lib/widgets/add_item_page.dart
@@ -1,48 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:reminding_notes/models/note_view_model.dart';
 
-// TODO: continue implementation
 class AddItemPage extends StatelessWidget {
   const AddItemPage({Key? key}) : super(key: key);
 
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2015, 8),
+      lastDate: DateTime(2101),
+    );
+    // TODO: set date
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    // TODO: set time
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: GestureDetector(
+                onTap: () {
+                  // TODO: add save action
+                },
+                child: const Icon(Icons.done_rounded, size: 26.0),
+              ),
+            ),
+          ],
+        ),
         body: Column(
           children: [
-            TextFormField(
-              initialValue: 'Event Title',
-              maxLength: 20,
-              decoration: InputDecoration(
-                icon: const Icon(Icons.edit_rounded),
-                labelText: 'Title',
-                labelStyle: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                ),
-                helperText: 'Event title',
-                suffixIcon: const Icon(
-                  Icons.check_circle_rounded,
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Theme.of(context).primaryColor),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                maxLength: 20,
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  helperText: 'Event title',
+                  suffixIcon: const Icon(
+                    Icons.check_circle_rounded,
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Theme.of(context).primaryColor),
+                  ),
                 ),
               ),
             ),
-            TextFormField(
-              initialValue: 'Event note',
-              maxLength: 20,
-              decoration: InputDecoration(
-                icon: const Icon(Icons.edit_rounded),
-                labelText: 'Note',
-                labelStyle: TextStyle(
-                  color: Theme.of(context).primaryColor,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                maxLength: 200,
+                minLines: 8,
+                maxLines: 21,
+                decoration: InputDecoration(
+                  labelText: 'Note',
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  helperText: 'Event note',
+                  suffixIcon: const Icon(
+                    Icons.check_circle_rounded,
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Theme.of(context).primaryColor),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Theme.of(context).primaryColor),
+                  ),
                 ),
-                helperText: 'Event note',
-                suffixIcon: const Icon(
-                  Icons.check_circle_rounded,
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Theme.of(context).primaryColor),
-                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () => _selectTime(context),
+                    child: Row(
+                      children: const [
+                        Icon(Icons.access_time_rounded),
+                        Text('Time'),
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => _selectDate(context),
+                    child: Row(
+                      children: const [
+                        Icon(Icons.event_rounded),
+                        Text('Date'),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  Expanded(
+                    child: DropdownButton<int>(
+                      value: 1,
+                      items: NoteType.values
+                          .asMap()
+                          .map(
+                            (i, monthName) =>
+                                MapEntry<int, DropdownMenuItem<int>>(
+                              i,
+                              DropdownMenuItem<int>(
+                                child: Text(
+                                  monthName.name.replaceFirst('Reminding', ''),
+                                ),
+                                value: i + 1,
+                              ),
+                            ),
+                          )
+                          .values
+                          .toList(),
+                      onChanged: (val) {
+                        // TODO: set note type action
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
