d65a37d14eb0444284df15e17f30e9b77b94eae9
stoian2662 <stoian2662@gmail.com>
2021-12-26 21:30:22 +0200

    GenericRepository added.

---
+++ lib/infrastructure/common/generic_repository.dart
@@ -0,0 +1,17 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class GenericRepository<T> {
+  late Box<T> itemsBox;
+
+  void init() async {
+    itemsBox = await Hive.openBox<T>('${T.toString()}-items');
+  }
+
+  Future add(T entry) => itemsBox.add(entry);
+
+  T? get(int id) => itemsBox.get(id);
+
+  List<T> getAll() => itemsBox.values.toList();
+
+  Future delete(int id) => itemsBox.delete(id);
+}
