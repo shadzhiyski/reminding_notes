03c2d98af20e948c7fab0e7ed3d1d93471e8ff94
stoian2662 <stoian2662@gmail.com>
2022-01-04 23:09:18 +0200

    boxNameAlias parameter added.

--- lib/infrastructure/common/generic_repository.dart
+++ lib/infrastructure/common/generic_repository.dart
@@ -2,8 +2,13 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 class GenericRepository<T> {
   bool _isInitialized = false;
-  final String boxName = '${T.toString()}-items';
+  final String boxNameAlias;
+  late String boxName = boxNameAlias.isEmpty
+      ? '${T.toString()}-items'
+      : '${T.toString()}-$boxNameAlias-items';
   late Box<T> itemsBox;
+
+  GenericRepository({this.boxNameAlias = ''});
 
   bool get isInitialized => _isInitialized;
 
@@ -15,6 +20,9 @@ class GenericRepository<T> {
   Future add(T entry) => itemsBox.add(entry);
 
   T? get(int id) => itemsBox.get(id);
+
+  List<T> getWhere(bool Function(T element) filter) =>
+      itemsBox.values.where(filter).toList();
 
   List<T> getAll() => itemsBox.values.toList();
 
