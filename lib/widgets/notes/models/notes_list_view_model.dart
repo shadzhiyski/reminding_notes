b518ac4105e6ef0fd31dfe1e716b1cefd926f846
stoian2662 <stoian2662@gmail.com>
2022-01-11 22:29:14 +0200

    currentLoadedDay added.

--- lib/widgets/notes/models/notes_list_view_model.dart
+++ lib/widgets/notes/models/notes_list_view_model.dart
@@ -5,14 +5,18 @@ import 'note_view_model.dart';
 
 class NotesListViewModel extends ChangeNotifier {
   final NotesService notesService;
+  late DateTime _currentLoadedDay;
   late List<NoteViewModel> _notes;
 
   NotesListViewModel({required this.notesService}) {
     _notes = [];
+    _currentLoadedDay = DateTime.now();
   }
 
+  DateTime get currentLoadedDay => _currentLoadedDay;
   List<NoteViewModel> get notes => _notes;
   Future loadNotes(DateTime day) async {
+    _currentLoadedDay = day;
     var result = await notesService.getNotesForDay(day);
     _notes = result
         .map(
