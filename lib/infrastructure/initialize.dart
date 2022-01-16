9ceefc9b053fc9818e0d85ab8ffe7a1e4ffd0a6f
stoian2662 <stoian2662@gmail.com>
2022-01-16 17:56:13 +0200

    Local notifications enabled.

--- lib/infrastructure/initialize.dart
+++ lib/infrastructure/initialize.dart
@@ -1,10 +1,30 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'notes/note_adapter.dart';
 import 'notes/reminding_adapter.dart';
 
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
 Future initializeInfrastructure() async {
+  await _initializeLocalDatabase();
+  await _initializeLocalNotifications();
+}
+
+Future _initializeLocalDatabase() async {
   await Hive.initFlutter();
   Hive.registerAdapter(NoteAdapter());
   Hive.registerAdapter(RemindingAdapter());
+}
+
+Future _initializeLocalNotifications() async {
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('launch_background');
+
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
 }
